e95a7774b2481b1bf9c78e5e838f08aa7abf5b5d
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2021-12-17 13:56:35 +0200

    all utf-8

--- task_5/task_5.cpp
+++ task_5/task_5.cpp
@@ -1,7 +1,7 @@
-/*
-5. Пользователь вводит с клавиатуры сумму в гривнах, срок вклада в
-месяцах и процентную ставку за год. Посчитать, сколько денег будет у
-пользователя.
+п»ї/*
+5. РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РІРІРѕРґРёС‚ СЃ РєР»Р°РІРёР°С‚СѓСЂС‹ СЃСѓРјРјСѓ РІ РіСЂРёРІРЅР°С…, СЃСЂРѕРє РІРєР»Р°РґР° РІ
+РјРµСЃСЏС†Р°С… Рё РїСЂРѕС†РµРЅС‚РЅСѓСЋ СЃС‚Р°РІРєСѓ Р·Р° РіРѕРґ. РџРѕСЃС‡РёС‚Р°С‚СЊ, СЃРєРѕР»СЊРєРѕ РґРµРЅРµРі Р±СѓРґРµС‚ Сѓ
+РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ.
 */
 
 #include<iostream>
@@ -11,27 +11,27 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, ""); //работает только с потоком вывода	
+	setlocale(LC_ALL, ""); //СЂР°Р±РѕС‚Р°РµС‚ С‚РѕР»СЊРєРѕ СЃ РїРѕС‚РѕРєРѕРј РІС‹РІРѕРґР°	
 
-	cout << "Введите сумму в гривнах: " << endl;
-	double amount_in_hryvnia{ 0.0 }; //сумма в гривнах
+	cout << "Р’РІРµРґРёС‚Рµ СЃСѓРјРјСѓ РІ РіСЂРёРІРЅР°С…: " << endl;
+	double amount_in_hryvnia{ 0.0 }; //СЃСѓРјРјР° РІ РіСЂРёРІРЅР°С…
 	cin >> amount_in_hryvnia;
 
-	cout << "Введите срок вклада в месяцах: " << endl;
-	int term_of_deposit_in_months{ 0 }; //срок вклада в месяцах
+	cout << "Р’РІРµРґРёС‚Рµ СЃСЂРѕРє РІРєР»Р°РґР° РІ РјРµСЃСЏС†Р°С…: " << endl;
+	int term_of_deposit_in_months{ 0 }; //СЃСЂРѕРє РІРєР»Р°РґР° РІ РјРµСЃСЏС†Р°С…
 	cin >> term_of_deposit_in_months;
 
-	cout << "Введите процентную ставку за год: " << endl;
-	double interest_rate_for_the_year{ 0.0 }; //процентная ставка за 1 год
+	cout << "Р’РІРµРґРёС‚Рµ РїСЂРѕС†РµРЅС‚РЅСѓСЋ СЃС‚Р°РІРєСѓ Р·Р° РіРѕРґ: " << endl;
+	double interest_rate_for_the_year{ 0.0 }; //РїСЂРѕС†РµРЅС‚РЅР°СЏ СЃС‚Р°РІРєР° Р·Р° 1 РіРѕРґ
 	cin >> interest_rate_for_the_year;
 
-	double monthly_interest_rate = interest_rate_for_the_year / 12; //процентная ставка за 1 месяц
+	double monthly_interest_rate = interest_rate_for_the_year / 12; //РїСЂРѕС†РµРЅС‚РЅР°СЏ СЃС‚Р°РІРєР° Р·Р° 1 РјРµСЃСЏС†
 
-	double month_1_income = amount_in_hryvnia / 100 * monthly_interest_rate; //доход за 1 месяц
-	//cout << "Доход за 1 м. составил: " << month_1_income << " грн." << endl;
+	double month_1_income = amount_in_hryvnia / 100 * monthly_interest_rate; //РґРѕС…РѕРґ Р·Р° 1 РјРµСЃСЏС†
+	//cout << "Р”РѕС…РѕРґ Р·Р° 1 Рј. СЃРѕСЃС‚Р°РІРёР»: " << month_1_income << " РіСЂРЅ." << endl;
 
-	double all_income = int((((amount_in_hryvnia / 100 * monthly_interest_rate) * term_of_deposit_in_months) + 0.005) * 100); //доход за весь период
-	cout << "Доход за " << term_of_deposit_in_months << " м. составил: " << all_income / 100 << " грн." << endl;
+	double all_income = int((((amount_in_hryvnia / 100 * monthly_interest_rate) * term_of_deposit_in_months) + 0.005) * 100); //РґРѕС…РѕРґ Р·Р° РІРµСЃСЊ РїРµСЂРёРѕРґ
+	cout << "Р”РѕС…РѕРґ Р·Р° " << term_of_deposit_in_months << " Рј. СЃРѕСЃС‚Р°РІРёР»: " << all_income / 100 << " РіСЂРЅ." << endl;
 
 	return 0;
 }
